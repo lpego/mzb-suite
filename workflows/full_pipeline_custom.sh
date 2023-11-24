46a6618cc7f6fd97f082a159575c3cbf29aa3c46
Luca Pegoraro <luca.pegoraro90@gmail.com>
2023-11-24 10:24:22 +0100

    fixed path for wokring with Renku virtual sessions

--- workflows/full_pipeline_custom.sh
+++ workflows/full_pipeline_custom.sh
@@ -1,19 +1,18 @@
 #!/bin/bash 
 
-# Definition of recurrent patterns
-
-ROOT_DIR="/work/mzb-workflow"
+## Definition of recurrent patterns
+## The root path is for working with notebooks on virtual sessions on Renkulab, your path may differ! 
+ROOT_DIR="/home/jovyan/work/mzb-workflow"
 MODEL_C="convnext-small-v0"
 MODEL_S="mit-b2-v0"
-## ------------------------------------------------------
 
+## ------------------------------------------------------
 python scripts/image_parsing/main_raw_to_clips.py \
     --input_dir=${ROOT_DIR}/data/mzb_example_data/raw_img/ \
     --output_dir=${ROOT_DIR}/data/mzb_example_data/derived/blobs/ \
     --save_full_mask_dir=${ROOT_DIR}/data/mzb_example_data/derived/full_image_masks_test \
-    --config_file=${ROOT_DIR}/configs/configuration_flume_datasets.yaml #\
-    # -v
-
+    --config_file=${ROOT_DIR}/configs/configuration_flume_datasets.yaml \
+    -v
 
 ## This is run to classify a custom folder structure and will regturn a csv with the results 
 ## (filename, predicted class, probability of prediction)
@@ -41,7 +40,6 @@ python scripts/skeletons/main_unsupervised_skeleton_estimation.py \
     --save_masks=${ROOT_DIR}/data/mzb_example_data/derived/skeletons/automatic_skeletons/ \
     --list_of_files=None \
     -v
-
 
 ## And this is to use a supervised model to parse skeletons. It predicts both body length and head width
 ## and stores the masks as images, and saves measurements into a csv
