b6a7fb78137e9c888acaaecd3a757895433ed2d1
michele <michele.volpi@sdsc.ethz.ch>
2023-12-08 08:38:54 +0100

    merging

--- workflows/run_finetune_classification.sh
+++ workflows/run_finetune_classification.sh
@@ -1,55 +1,58 @@
 #!/bin/bash
-# set up pipeline to fine tune the classification model
 
-ROOT_DIR="/data/shared/mzb-workflow"
-MODEL="convnext-small-vtest-2"
-LSET_FOLD=$ROOT_DIR/data/learning_sets/project_portable_flume/aggregated_learning_sets
+## This workflow sets up and runs the finetuning of the classification model
+## The root path specified here in ROOT_DIR is for working with notebooks on virtual sessions on Renkulab, your path may differ! 
+ROOT_DIR="/home/jovyan/work/mzb-workflow"
+MODEL="efficientnet-b2-v0"
+LSET_FOLD=${ROOT_DIR}/data/mzb_example_data/aggregated_set
 
-# OUTPUT_DIR_CLASSIFICATION_INFERENCE=$ROOT_DIR/results/project_portable_flume/classification/classification_inference
-
-
-###############################################################
-## This has to be run once, to create the curated learning sets
-###############################################################
-
+## This runs only if the target aggregate learning sets folder doesn't exist yet, 
+## and then creates it based on the specified taxonomic rank and taxonomy file. 
+## --------------------------------------------------------------------------------
 if [ -d "${LSET_FOLD}" ];
 then
     echo "Directory ${LSET_FOLD} exists." 
 else 
     echo "Directory ${LSET_FOLD} is being set up."
+    ## this creates the aggregated training set
     python ${ROOT_DIR}/scripts/classification/main_prepare_learning_sets.py \
-    --input_dir=${ROOT_DIR}/data/learning_sets/project_portable_flume/curated_learning_sets/ \
-    --taxonomy_file=${ROOT_DIR}/data/MZB_taxonomy.csv \
-    --output_dir=${ROOT_DIR}/data/learning_sets/project_portable_flume/aggregated_learning_sets \
-    --config_file=${ROOT_DIR}/configs/configuration_flume_datasets.yaml \
+    --input_dir=${ROOT_DIR}/data/mzb_example_data/training_dataset/trn_set/ \
+    --taxonomy_file=${ROOT_DIR}/data/mzb_example_data/MZB_taxonomy.csv \
+    --output_dir=${ROOT_DIR}/data/mzb_example_data/aggregated_set/trn_set/ \
+    --config_file=${ROOT_DIR}/configs/mzb_example_config.yaml \
+    # -v
+    ## this created the aggreagted validation set
+    python ${ROOT_DIR}/scripts/classification/main_prepare_learning_sets.py \
+    --input_dir=${ROOT_DIR}/data/mzb_example_data/training_dataset/val_set/ \
+    --taxonomy_file=${ROOT_DIR}/data/mzb_example_data/MZB_taxonomy.csv \
+    --output_dir=${ROOT_DIR}/data/mzb_example_data/aggregated_set/val_set/ \
+    --config_file=${ROOT_DIR}/configs/mzb_example_config.yaml \
     # -v
 fi
 
-# This is run to fine tune the classification model. It will read the curated learning sets and will return a new model
-# ---------------------------------------------------------------------------------------------------------------
-python $ROOT_DIR/scripts/classification/main_classification_finetune.py \
-    --config_file=$ROOT_DIR/configs/configuration_flume_datasets.yaml \
-    --input_dir=$ROOT_DIR/data/learning_sets/project_portable_flume/aggregated_learning_sets/ \
-    --save_model=$ROOT_DIR/models/mzb-classification-models/$MODEL \
+## This is run to finetune the classification model based on the new aggreagted learning sets; it will return a new model.
+## --------------------------------------------------------------------------------
+python ${ROOT_DIR}/scripts/classification/main_classification_finetune.py \
+    --input_dir=${ROOT_DIR}/data/mzb_example_data/aggregated_set/trn_set/ \
+    --save_model=${ROOT_DIR}/models/mzb-classification-models/${MODEL}_aggregated \
+    --config_file=${ROOT_DIR}/configs/mzb_example_config.yaml \
     # -v
 
-# This is run to classify a custom folder structure and will regturn a csv with the results
-# Specifically, this is run on the validation set to get the accuracy of the model
-# ------------------------------------------------------------------------------------------
+## This uses the newly trained model to classify new images; 
+## specifically, this runs on the validation set to get the accuracy of the model
+## --------------------------------------------------------------------------------
 python ${ROOT_DIR}/scripts/classification/main_classification_inference.py \
-    --config_file=${ROOT_DIR}/configs/configuration_flume_datasets.yaml \
-    --input_dir=${ROOT_DIR}/data/learning_sets/project_portable_flume/aggregated_learning_sets/val_set \
-    --input_model=${ROOT_DIR}/models/mzb-classification-models/${MODEL} \
-    --output_dir=${ROOT_DIR}/results/project_portable_flume/classification/class_${MODEL}_validation_set/ \
+    --input_dir=${ROOT_DIR}/data/mzb_example_data/aggregated_set/val_set/ \
+    --input_model=${ROOT_DIR}/models/mzb-classification-models/${MODEL}_aggregated \
+    --output_dir=${ROOT_DIR}/results/mzb_example_data/classification/${MODEL}_aggregated/ \
+    --config_file=${ROOT_DIR}/configs/mzb_example_config.yaml \
     # -v
 
-# And this is to classify the mixed set, a custom folder structure with images from different sources
-# ---------------------------------------------------------------------------------------------------
-python ${ROOT_DIR}/scripts/classification/main_classification_inference.py \
-    --config_file=${ROOT_DIR}/configs/configuration_flume_datasets.yaml \
-    --input_dir=${ROOT_DIR}/data/learning_sets/project_portable_flume/aggregated_learning_sets/mixed_set \
-    --input_model=${ROOT_DIR}/models/mzb-classification-models/${MODEL} \
-    --output_dir=${ROOT_DIR}/results/project_portable_flume/classification/class_${MODEL}_external_set/ \
-    # -v
-
-# --> the classifier using a ConvNext or EfficientNet-b2 model is best, so we will use that one probably. 
+# ## And this can be adapted to your own external set of iamges, a custom folder structure with images from different sources
+# ## --------------------------------------------------------------------------------
+# python ${ROOT_DIR}/scripts/classification/main_classification_inference.py \
+#     --input_dir=${ROOT_DIR}/data/YOUR_IMAGE_SET_HERE/ \
+#     --input_model=${ROOT_DIR}/models/mzb-classification-models/${MODEL}_aggregated \
+#     --output_dir=${ROOT_DIR}/results/mzb_example_data/classification/${MODEL}_aggregated_external/ \
+#     --config_file=${ROOT_DIR}/configs/mzb_example_config.yaml \
+#     # -v
