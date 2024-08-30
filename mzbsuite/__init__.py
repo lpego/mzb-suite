2f8545177c9ab6b45d9b448a8c57a1567b2be6e2
Luca Pegoraro <luca.pegoraro90@gmail.com>
2024-08-30 17:24:06 +0200

    Bumping up minor minor version

--- mzbsuite/__init__.py
+++ mzbsuite/__init__.py
@@ -1,7 +1,7 @@
 # Global variables for mzbsuite
 
 __project_name__ = "mzbsuite"
-__version__ = "0.2.1"
+__version__ = "0.2.3"
 __version_full__ = __version__
 __author__ = "Michele Volpi, Luca Pegoraro"
-__date__ = "2023-11-23"
+__date__ = "2024-08-28"
