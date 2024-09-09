c1b3b3246480d06131259e161040975bb1f1eba2
Luca Pegoraro <luca.pegoraro90@gmail.com>
2024-09-09 17:33:01 +0200

    Updating mzbsuite version for Streamlit GUI

--- mzbsuite/__init__.py
+++ mzbsuite/__init__.py
@@ -1,7 +1,7 @@
 # Global variables for mzbsuite
 
 __project_name__ = "mzbsuite"
-__version__ = "0.2.3"
+__version__ = "0.3.0"
 __version_full__ = __version__
 __author__ = "Michele Volpi, Luca Pegoraro"
 __date__ = "2024-09-09"
