3ad376ec39c7791e3095c8b0be04b92350dcea8f
Luca Pegoraro <luca.pegoraro90@gmail.com>
2024-09-13 18:11:35 +0200

    Configuring landing page and sections using st-pages

--- streamlit_homepage.py
+++ streamlit_homepage.py
@@ -1,38 +1,32 @@
 import streamlit as st
 from st_pages import add_page_title, get_nav_from_toml
 
-st.set_page_config(layout="wide", page_title="mzb-suite")
+import streamlit as st
+from st_pages import add_page_title, get_nav_from_toml
 
-st.write("# Welcome to mzb-suite!")
+st.set_page_config(
+    page_title="mzbsuite",
+    # page_icon="🧊",   
+    layout="wide",
+    initial_sidebar_state="expanded",
+    # menu_items={
+    #     'Get Help': 'https://www.extremelycoolapp.com/help',
+    #     'Report a bug': "https://www.extremelycoolapp.com/bug",
+    #     'About': "# This is a header. This is an *extremely* cool app!"
+    # }
+)
+
+st.logo("docs/assets/mzbsuite_logo_v2.1.svg")
+
+st.sidebar.success("Select a task above.")
 
 # If you want to use the no-sections version, this
 # defaults to looking in .streamlit/pages.toml, so you can
 # just call `get_nav_from_toml()`
 nav = get_nav_from_toml(".streamlit/pages_sections.toml")
 
-st.logo("docs/assets/mzbsuite_logo_v2.1.svg")
-
 pg = st.navigation(nav)
 
-# st.sidebar.success("Select a task above.")
-
 add_page_title(pg)
-
-st.markdown(
-    """
-    mzb-suite is an image processing pipeline for lab images of macrozoobenthos (MZB), partially automating data extraction from images.
-    **Select your task from the sidebar**
-    # 
-    # 
-    #
-    # 
-    ### Resources and references
-    - User Interface made with [streamlit.io](https://streamlit.io)
-    - Inspired by [DeepMeerkat](http://benweinstein.weebly.com/deepmeerkat.html)
-    # 
-    # 
-    ##### If you have questions contact [Luca.Pegoraro@WSL.ch](mailto:Luca.Pegoraro@WSL.ch)
-    """
-)
 
 pg.run()
---
+++ streamlit_pages/app_homepage.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+st.title ('Welcome to mzbsuite!')
+
+st.markdown(
+    """
+    mzb-suite is an image processing pipeline for lab images of macrozoobenthos (MZB), partially automating data extraction from images.
+    
+    Find the main repository here: https://gitlab.renkulab.io/biodetect/mzb-workflow
+    
+    Read the documentation here: https://mzb-workflow.readthedocs.io/en/latest/?badge=latest
+    #
+    **👈 Select your task from the sidebar**
+    # 
+    ### Resources and references
+    - User Interface made with [streamlit.io](https://streamlit.io)
+    """
+)
