6174daa163b3d6055736da37419b1786a2b475fd
Luca Pegoraro <luca.pegoraro90@gmail.com>
2024-09-13 17:35:59 +0200

    Adapting homepage to play nice with st-pages

--- streamlit_homepage.py
+++ streamlit_homepage.py
@@ -1,12 +1,22 @@
 import streamlit as st
+from st_pages import add_page_title, get_nav_from_toml
 
-st.set_page_config(
-    page_title="mzb-suite",
-)
+st.set_page_config(layout="wide", page_title="mzb-suite")
 
-st.write("# Welcome to mzb-suite")
+st.write("# Welcome to mzb-suite!")
 
-st.sidebar.success("Select a task above.")
+# If you want to use the no-sections version, this
+# defaults to looking in .streamlit/pages.toml, so you can
+# just call `get_nav_from_toml()`
+nav = get_nav_from_toml(".streamlit/pages_sections.toml")
+
+st.logo("docs/assets/mzbsuite_logo_v2.1.svg")
+
+pg = st.navigation(nav)
+
+# st.sidebar.success("Select a task above.")
+
+add_page_title(pg)
 
 st.markdown(
     """
@@ -24,3 +34,5 @@ st.markdown(
     ##### If you have questions contact [Luca.Pegoraro@WSL.ch](mailto:Luca.Pegoraro@WSL.ch)
     """
 )
+
+pg.run()
