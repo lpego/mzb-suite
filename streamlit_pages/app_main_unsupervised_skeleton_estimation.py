b2d81a139fbee6454475f23807dc0214a6e128e3
Luca Pegoraro <luca.pegoraro90@gmail.com>
2025-01-13 18:05:10 +0100

    Revised logic, polished and commented a bit

--- streamlit_pages/app_main_unsupervised_skeleton_estimation.py
+++ streamlit_pages/app_main_unsupervised_skeleton_estimation.py
@@ -3,6 +3,7 @@ import time, datetime
 import tkinter as tk
 from tkinter import filedialog
 import yaml
+from pathlib import Path
 
 ### streamlit utils
 import streamlit as st
@@ -12,7 +13,7 @@ import mzbsuite.streamlit.streamlit_utils as stutils
 from mzbsuite.utils import cfg_to_arguments, noneparse
 from scripts.skeletonization.main_unsupervised_skeleton_estimation import main
 
-# Configuration by variables
+# Configuration by declaring variables
 VARIABLES = {
     "config_file": {
         "type": "file",
@@ -43,6 +44,7 @@ for var, props in VARIABLES.items():
 # st.title("Unsupervised skeletonization estimation")
 st.write("Please provide the following inputs:")
 
+# Logic for 2-step file selection or single step folder selector
 for var, props in VARIABLES.items():
     if props.get("two_stage"):  # Two-stage file selector
         col1, col2 = st.columns([2, 4])
@@ -51,8 +53,8 @@ for var, props in VARIABLES.items():
             st.write(f"**{props['description']}**")
             if st.button(props["stage1_label"], key=f"{var}_folder_button"):
                 st.session_state[f"{var}_folder"] = stutils.select_folder()
-            if st.session_state.get(f"{var}_folder"):
-                st.write(f"Folder selected: `{st.session_state[f'{var}_folder']}`")
+            # if st.session_state.get(f"{var}_folder"):
+            #     st.write(f"Folder selected: `{st.session_state[f'{var}_folder']}`")
 
         with col2:
             if st.session_state.get(f"{var}_folder"):
@@ -62,8 +64,8 @@ for var, props in VARIABLES.items():
                 selected_file = st.selectbox(f"Select a file for {var}", files, key=f"{var}_file")
                 if selected_file:
                     st.session_state[var] = os.path.join(st.session_state[f"{var}_folder"], selected_file)
-                if st.session_state.get(var):
-                    st.write(f"File selected: `{st.session_state[var]}`")
+                # if st.session_state.get(var):
+                #     st.write(f"File selected: `{st.session_state[var]}`")
 
     else:  # Regular single-stage selectors
         st.write(f"**{props['description']}**")
@@ -84,38 +86,42 @@ start_time = datetime.datetime.now()
 
 # Buttons: Finish and Clear
 col1, col2 = st.columns(2)
+launch_pressed = False
 
 with col1:
-    if st.button("Finish"):
-        if stutils.validate_session_state(VARIABLES):
-            
-            # args = {key: st.session_state[key] for key in st.session_state}
-            args = stutils.log_session_state(start_time, VARIABLES) # just use the log dictionary
-            args.pop("start_time", None) # remove unnecessary key:value pairs
-            args.pop("end_time", None) # remove unnecessary key:value pairs
-            args = cfg_to_arguments(args)
-            # st.write(args)
-            
-            with open('D:\mzb-workflow\configs\mzb_example_config.yaml', "r") as f:
-                cfg = yaml.load(f, Loader=yaml.FullLoader)
-            cfg = cfg_to_arguments(cfg)
-            # st.write(cfg)
-            
-            ### Finally, launch the script!
-            main(args, cfg, st_GUI=True) # st_GUI flag that is launched from GUI
-            
-            # Save log
-            streamlit_log = stutils.log_session_state(start_time, VARIABLES)
-            with open("streamlit_log.yaml", "a") as outfile:
-                yaml.dump(streamlit_log, outfile, sort_keys=False)
-
-            st.write('**:green[Success!]** Run parameters saved to "streamlit_log.yaml".')
-        else:
-            st.write("**:red[Please complete all required inputs.]**")
-
+    if st.button("Launch"):
+        launch_pressed = True            
+    # else:
+    #     st.write("**:red[Please complete all required inputs.]**")
 with col2:
     if st.button("Clear"):
         stutils.clear_session_state(VARIABLES)  # Reset variables and refresh the UI
+
+# Launch script if all arguments are collected
+if launch_pressed: 
+    if stutils.validate_session_state(VARIABLES):
+        # args = {key: st.session_state[key] for key in st.session_state}
+        args = stutils.log_session_state(start_time, VARIABLES) # just use the log dictionary
+        args.pop("start_time", None) # remove unnecessary key:value pairs
+        args.pop("end_time", None) # remove unnecessary key:value pairs
+        # st.write(args)
+        
+        with open(args["config_file"], "r") as f:
+            cfg = yaml.load(f, Loader=yaml.FullLoader)
+        
+        args = cfg_to_arguments(args)
+        cfg = cfg_to_arguments(cfg)
+        # st.write(cfg)
+        
+        # finally, call the script
+        main(args, cfg, st_GUI=True) # st_GUI flag that is launched from GUI
+        
+        # Save log
+        streamlit_log = stutils.log_session_state(start_time, VARIABLES)
+        with open("streamlit_log.yaml", "a") as outfile:
+            yaml.dump(streamlit_log, outfile, sort_keys=False)
+
+        st.write('**:green[Success!]** Run parameters saved to "streamlit_log.yaml".')
 
 # Debugging info
 if st.session_state.get("verbose"):
