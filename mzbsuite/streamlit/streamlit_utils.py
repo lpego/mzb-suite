169f64253056bc01156e4cbc9ca8538945c7dd1e
Luca Pegoraro <luca.pegoraro90@gmail.com>
2025-01-13 17:38:22 +0100

    Added other helper functions for streamlit_pages scripts, wrote function descriptions

--- mzbsuite/streamlit/streamlit_utils.py
+++ mzbsuite/streamlit/streamlit_utils.py
@@ -2,70 +2,81 @@ import os
 import time, datetime
 import tkinter as tk
 from tkinter import filedialog
-import streamlit as st
 import yaml
+import streamlit as st
 
-###
 def select_folder(): 
-   root = tk.Tk()
-   folder_path = filedialog.askdirectory(master=root)
-   root.destroy()
-   return folder_path
+    """
+    Prompt user to select folder from filesystem
+    """
+    root = tk.Tk()
+    folder_path = filedialog.askdirectory(master=root)
+    root.destroy()
+    return folder_path
 
 def file_selector(folder_path):
+    """
+    Button to list all files in selected folder and display in dropdown; 
+    works in combination with select_folder.
+    """
     filenames = os.listdir(folder_path)
     selected_filename = st.selectbox('Select a file', filenames)
     return os.path.join(folder_path, selected_filename)
 
-def clear():
-    st.session_state.folder_path=None
-    st.session_state.model_path=None
-    st.session_state.config_file=None
-    st.session_state.folder_path3=None
-    st.session_state.results_path=None
-    st.session_state.config_file=None
-    st.session_state.threshold=0
-    st.session_state.model_select=""
-    
-def checkEmpty():   
-    results_var=''
-    config_var=''
-    # update_var=''
-    # aggregate_var=''
-    # plots_var= ''
-    # threshold_var=''
-    try:
-        results_var = st.session_state.results_path
-    except:
-        st.write('**:red[Please select folder where results / scores are stored]**')
-    try:
-        config_var = st.session_state.config_file
-    except:
-        st.write('**:red[Please select path to the configuration file]**')
-    # try:
-    #     update_var = st.session_state.update
-    # except:
-    #     st.write('**:red[Please select whther to update metrics for all videos]**')
-    # try:
-    #     aggregate_var = st.session_state.aggregate
-    # except:
-    #     st.write('**:red[Please select whether to aggregate metrics per folder]**')
-    # try:
-    #     plots_var = st.session_state.plots
-    # except:
-    #     st.write('**:red[Please select whether to plot metrics]**')
-    # try: 
-    #     threshold != 0 | threshold != None
-    # except: 
-    #     st.write('**:red[Please select a threshold]**')
-    if (
-        results_var != ''
-        and config_var != ''
-        # and update_var != ''
-        # and aggregate_var != ''
-        # and plots_var != ''
-        # and threshold != None
-    ):
-        return True
-    else:
+### Functions below require configuration by variables
+### normally declared at the top of the streamlit page script
+### Example below: 
+# VARIABLES = {
+#     "config_file": {
+#         "type": "file",
+#         "description": "Path to config file",
+#         "two_stage": True,
+#         "stage1_label": "Select folder for config file",
+#         "stage2_label": "Select config file",
+#     },
+#     "input_dir": {"type": "folder", "description": "Directory containing the masks"},
+#     "output_dir": {"type": "folder", "description": "Directory to save results"},
+#     "save_masks": {"type": "folder", "description": "Directory to save masks as JPG"},
+#     "list_of_files": {
+#         "type": "file",
+#         "description": "CSV file with classification predictions",
+#         "two_stage": True,
+#         "stage1_label": "Select folder for list of files",
+#         "stage2_label": "Select list of files",
+#     },
+#     "verbose": {"type": "boolean", "description": "Verbose mode"},
+# }
+### Admissible types are "file", "folder", "boolean"
+### When two_stage is True, file_selector is called after select_folder. 
+
+def clear_session_state(VARIABLES):
+    """
+    Button to clear all user inputs from UI; forces app reload. 
+    """
+    for var in VARIABLES.keys():
+        st.session_state[var] = None
+        if VARIABLES[var].get("two_stage"):
+            st.session_state[f"{var}_folder"] = None
+    st.rerun()  # Refresh the UI after clearing
+
+def validate_session_state(VARIABLES):
+    """
+    Checks that all required states have been assigned by user input, 
+    otherwise returns an error and list of missing requirements. 
+    """
+    missing = [var for var in VARIABLES.keys() if var != "verbose" and not st.session_state.get(var)]
+    if missing:
+        st.write(f"**:red[Missing required inputs: {', '.join(missing)}]**")
         return False
+    return True
+
+def log_session_state(start_time, VARIABLES):
+    """
+    Returns YAML-like logs as dictionary for debug; saves started and end time of run tooo. 
+    """
+    streamlit_log = {
+        "start_time": start_time,
+        **{var: st.session_state[var] for var in VARIABLES.keys()},
+        "end_time": datetime.datetime.now(),
+    }
+    return streamlit_log
