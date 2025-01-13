e30cfac6286f97b594356428fa1bfd80ac7ce997
Luca Pegoraro <luca.pegoraro90@gmail.com>
2025-01-13 18:17:14 +0100

    Moved to configuration-based declaration

--- streamlit_pages/app_main_raw_to_clips.py
+++ streamlit_pages/app_main_raw_to_clips.py
@@ -2,150 +2,119 @@ import os
 import time, datetime
 import tkinter as tk
 from tkinter import filedialog
-import streamlit as st
 import yaml
 
 ### streamlit utils
-from mzbsuite.streamlit.streamlit_utils import file_selector, select_folder
+import streamlit as st
+import mzbsuite.streamlit.streamlit_utils as stutils
 
 ### mzbsuite utils and scripts
 from mzbsuite.utils import cfg_to_arguments, noneparse
-from scripts.image_parsing.main_raw_to_clips import main
+from scripts.segmentation.main_raw_to_clips import main
 
-st.title ('Segmentation: main_raw_to_clips')
+# Configuration by declaring variables
+VARIABLES = {
+    "config_file": {
+        "type": "file",
+        "description": "Path to config file",
+        "two_stage": True,
+        "stage1_label": "Select folder for config file",
+        "stage2_label": "Select config file",
+    },
+    "input_dir": {"type": "folder", "description": "Directory containing the raw images"},
+    "output_dir": {"type": "folder", "description": "Directory where to save segmented clips"},
+    "save_full_mask_dir": {"type": "folder", "description": "Directory to save full-image masks as JPG"},
+    "verbose": {"type": "boolean", "description": "Verbose mode"},
+}
 
-### Custom functions
-# def clear():
-#     st.session_state.config_file=False
-#     st.session_state.input_dir=False
-#     st.session_state.output_dir=None
-#     st.session_state.save_full_mask_dir=None
+# Initialize session state for all variables
+for var, props in VARIABLES.items():
+    if var not in st.session_state:
+        st.session_state[var] = None
 
-def checkEmpty():
-   config_main_var=''
-   input_main_var=''
-   output_main_var=''
-   save_full_mask_dir_var=''
-   try:
-      config_main_var = st.session_state.config_file
-   except:
-      st.write('**:red[Please select path to config file with per-script args]**')
-   try:
-      input_main_var = st.session_state.input_dir
-   except:
-      st.write('**:red[Please select path to directory with raw images]**')
-   try:
-      output_main_var = st.session_state.output_dir
-   except:
-      st.write('**:red[Please select path to where to save model checkpoints]**')
-   try:
-      save_full_mask_dir_var = st.session_state.save_full_mask_dir
-   except:
-      st.write('**:red[Please select path to directory where to save labeled full masks]**')
-   if  config_main_var != '' and save_full_mask_dir_var !='' and input_main_var != '' and output_main_var != '':
-      return True
-   else:
-      return False
+# Display fields based on config
+# st.title("Unsupervised skeletonization estimation")
+st.write("Please provide the following inputs:")
 
-debug = True # set to False for regular operation
+# Logic for 2-step file selection or single step folder selector
+for var, props in VARIABLES.items():
+    if props.get("two_stage"):  # Two-stage file selector
+        col1, col2 = st.columns([2, 4])
 
-### Initialise YAML log
-streamlit_log = {"app_main_raw_to_clips": {"start_time": datetime.datetime.now()}}
-   
-### Actual variables to grab
-# Config file nested folder-file select
-col1, col2 = st.columns([2, 4])
-# Set up nested buttons states
-if not "config_folder" in st.session_state:
-    st.session_state["config_folder"] = False
-# Select config folder
-with col1: 
-    config_folder = st.session_state.get("config_folder", None)
-    st.write("Select folder where config files are stored")
-    config_folder_button = st.button("Select config folder")
-    if config_folder_button:
-        config_folder = select_folder()
-        st.session_state.config_folder = config_folder
-# Select file in config_folder
+        with col1:
+            st.write(f"**{props['description']}**")
+            if st.button(props["stage1_label"], key=f"{var}_folder_button"):
+                st.session_state[f"{var}_folder"] = stutils.select_folder()
+            # if st.session_state.get(f"{var}_folder"):
+            #     st.write(f"Folder selected: `{st.session_state[f'{var}_folder']}`")
+
+        with col2:
+            if st.session_state.get(f"{var}_folder"):
+                st.write(f"**{props['stage2_label']} in `{st.session_state[f'{var}_folder']}`**")
+                # Dropdown for file selection from the folder
+                files = [f for f in os.listdir(st.session_state[f"{var}_folder"]) if os.path.isfile(os.path.join(st.session_state[f"{var}_folder"], f))]
+                selected_file = st.selectbox(f"Select a file for {var}", files, key=f"{var}_file")
+                if selected_file:
+                    st.session_state[var] = os.path.join(st.session_state[f"{var}_folder"], selected_file)
+                # if st.session_state.get(var):
+                #     st.write(f"File selected: `{st.session_state[var]}`")
+
+    else:  # Regular single-stage selectors
+        st.write(f"**{props['description']}**")
+        if props["type"] == "file":
+            if st.button(f"Select {props['description']}", key=f"{var}_button"):
+                st.session_state[var] = stutils.file_selector()
+        elif props["type"] == "folder":
+            if st.button(f"Select {props['description']}", key=f"{var}_button"):
+                st.session_state[var] = stutils.select_folder()
+        elif props["type"] == "boolean":
+            st.session_state[var] = st.checkbox(props['description'])
+
+        if st.session_state[var]:
+            st.write(f"Selected: `{st.session_state[var]}`")
+
+# Start time
+start_time = datetime.datetime.now()
+
+# Buttons: Finish and Clear
+col1, col2 = st.columns(2)
+launch_pressed = False
+
+with col1:
+    if st.button("Launch"):
+        launch_pressed = True            
+    # else:
+    #     st.write("**:red[Please complete all required inputs.]**")
 with col2:
-    if st.session_state.config_folder is not False:
-        config_file = file_selector(config_folder)
-        st.session_state.config_file = config_folder
-        st.write('You selected `%s`' % config_file)
-        streamlit_log["app_main_raw_to_clips"]["config_file"] = config_file
+    if st.button("Clear"):
+        stutils.clear_session_state(VARIABLES)  # Reset variables and refresh the UI
 
-### Grab the other running parameters
-input_dir = st.session_state.get("input_dir", None)
-input_button = st.button("Select path to directory with raw images")
-if input_button:
-  input_dir = select_folder()
-  st.session_state.input_dir = input_dir
+# Launch script if all arguments are collected
+if launch_pressed: 
+    if stutils.validate_session_state(VARIABLES):
+        # args = {key: st.session_state[key] for key in st.session_state}
+        args = stutils.log_session_state(start_time, VARIABLES) # just use the log dictionary
+        args.pop("start_time", None) # remove unnecessary key:value pairs
+        args.pop("end_time", None) # remove unnecessary key:value pairs
+        # st.write(args)
+        
+        with open(args["config_file"], "r") as f:
+            cfg = yaml.load(f, Loader=yaml.FullLoader)
+        
+        args = cfg_to_arguments(args)
+        cfg = cfg_to_arguments(cfg)
+        # st.write(cfg)
+        
+        # finally, call the script
+        main(args, cfg, st_GUI=True) # st_GUI flag that is launched from GUI
+        
+        # Save log
+        streamlit_log = stutils.log_session_state(start_time, VARIABLES)
+        with open("streamlit_log.yaml", "a") as outfile:
+            yaml.dump(streamlit_log, outfile, sort_keys=False)
 
-if input_dir:
-   st.write("Selected folder path: `%s`" % input_dir)
-   streamlit_log["app_main_raw_to_clips"]["input_dir"] = input_dir
+        st.write('**:green[Success!]** Run parameters saved to "streamlit_log.yaml".')
 
-output_dir = st.session_state.get("output_dir", None)
-save_button = st.button("Select path to where to save the segmented clips of organisms")
-if save_button:
-  output_dir = select_folder()
-  st.session_state.output_dir = output_dir
-
-if output_dir:
-   st.write("Selected folder path: `%s`" % output_dir)
-   streamlit_log["app_main_raw_to_clips"]["save_model"] = output_dir
-
-save_full_mask_dir = st.session_state.get("save_full_mask_dir", None)
-input_button = st.button("Select path to directory where to save labeled full masks")
-if input_button:
-  save_full_mask_dir = select_folder()
-  st.session_state.save_full_mask_dir = save_full_mask_dir
-
-if save_full_mask_dir:
-   st.write("Selected folder path: `%s`" % save_full_mask_dir)
-   streamlit_log["app_main_raw_to_clips"]["save_full_mask_dir"] = save_full_mask_dir
-
-verbose = st.session_state.get("verbose", None)
-verbose_checkbox = st.checkbox("Show more info")
-if verbose_checkbox:
-    verbose = True
-else: 
-    verbose = False
-st.session_state.verbose = verbose
-streamlit_log["app_main_raw_to_clips"]["verbose"] = verbose
-
-### Debug info
-if debug: 
-   st.write("DEBUGGING INFO:", streamlit_log)
-
-### Launch button
-loadingButton = st.button('Launch!')
-
-if loadingButton and checkEmpty():
-   ### Create dictionary for argparse
-   arg_list = ["input_dir",
-               "output_dir",
-               "save_full_mask_dir", 
-               "verbose"
-               ]
-   args = {}
-   for x in arg_list: 
-      args[x] = eval(x)
-   
-   ### Convert to arguments
-   args = cfg_to_arguments(args)
-   
-   with open(str(config_file), "r") as f:
-      cfg = yaml.load(f, Loader=yaml.FullLoader)
-      
-   cfg = cfg_to_arguments(cfg)
-
-   ### Finally, launch the script!
-   main(args, cfg, st_GUI=True) # st_GUI flag that is launched from GUI
-
-   ### Write out the YAML logs
-   streamlit_log["app_main_raw_to_clips"]["end_time"] = datetime.datetime.now()
-   with open('streamlit_log.yaml', 'a') as outfile:
-      yaml.dump(streamlit_log, outfile, sort_keys=False)
-   del(streamlit_log) # empty the log, ready for next iteration
-   st.write('Success! Appending run parameters to "streamlit_log.yaml"')
+# Debugging info
+if st.session_state.get("verbose"):
+    st.write("DEBUG INFO:", {var: st.session_state[var] for var in VARIABLES.keys()})
